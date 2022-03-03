90de48031279120adb3aceca1d0e915b2efb4933
Kylie <kylie@comets.kr>
2022-03-03 09:51:31 +0900

    Testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundError } from 'rxjs';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -15,4 +16,33 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('shuld return a movie', () => {
+      service.create({
+        title: 'Test Movie',
+        genres: ['test'],
+        year: 2022,
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+  });
+
+  // it('shuld throw 404 error', () => {
+  //   try {
+  //     service.getOne(999);
+  //   } catch (e) {
+  //     expect(e).toBeInstanceOf(NotFoundError);
+  //     expect(e.message).toEqual('Movie with ID not found');
+  //   }
+  // });
 });
